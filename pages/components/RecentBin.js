c5d2ac74bb6fadb41a744e7b238f7257174e7654
Deniz <denizhincal05@gmail.com>
2022-02-16 09:52:17 +0300

    Logged Out Home Page Style

---
+++ pages/components/RecentBin.js
@@ -0,0 +1,19 @@
+import '../index'
+import React from 'react'
+import next from 'next'
+import Image from 'next/image'
+
+function RecentBin({ recentBinProf, binDesc}){
+    return(
+     <div className="flex p-2 bg-gray-900 mt-0.5">
+        <Image className="rounded-full" src= {require('/public/profilePic.png')} alt="avatar" width="48" height="48"/>
+         <div className="text-white ml-2" style={{fontFamily:'Consolas'}}>
+            <h2>{recentBinProf}</h2>
+            <a href='#' className="text-gray-600 hover:underline">{binDesc}</a>
+         </div>
+     </div>   
+    )
+}
+
+
+export default RecentBin;
