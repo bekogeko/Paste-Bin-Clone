e3b1afaccf966fe6259e74f009513d8841ff8ec9
Deniz <denizhincal05@gmail.com>
2022-02-16 19:09:03 +0300

    Change Components Dir / Add Truncate to Bin Description

--- components/RecentBin.js
+++ components/RecentBin.js
@@ -1,7 +1,14 @@
-import '../index'
+import '../pages/index'
 import React from 'react'
 import next from 'next'
 import Image from 'next/image'
+
+function Truncate(parag){
+    if(parag.length >= 20){
+        var truncated = parag.substring(0,20);
+        return truncated + "..."
+    }
+}
 
 function RecentBin({ recentBinProf, binDesc}){
     return(
@@ -9,7 +16,7 @@ function RecentBin({ recentBinProf, binDesc}){
         <Image className="rounded-full" src= {require('/public/profilePic.png')} alt="avatar" width="48" height="48"/>
          <div className="text-white ml-2" style={{fontFamily:'Consolas'}}>
             <h2>{recentBinProf}</h2>
-            <a href='#' className="text-gray-600 hover:underline">{binDesc}</a>
+            <a href='#' className="text-gray-600 hover:underline">{Truncate(binDesc)}</a>
          </div>
      </div>   
     )
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import RecentBin from './components/RecentBin'
+import RecentBin from '../components/RecentBin'
 
 export default function Home() {
   return (
